a203577a630613ec5e5d35a0a25b5b17f010d6f4
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 19:50:35 -0400

    Saving map_plots.R

--- map_plots.R
+++ map_plots.R
@@ -1,5 +1,6 @@
 
 library("ggplot2")
+df_final$stateName<-tolower(df_final$stateName)
 map_areaColor<-ggplot(df_final,aes(map_id=stateName)) +
            geom_map(map=us,aes(fill=stateArea)) +
            expand_limits(x=df_final$long,y=df_final$lat) + 
