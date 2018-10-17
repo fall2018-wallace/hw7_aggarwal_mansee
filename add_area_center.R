b4c4aff905cf50d0606541da58c284a7c50f8441
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 18:44:35 -0400

    Saving add_area_center.R

--- add_area_center.R
+++ add_area_center.R
@@ -2,3 +2,5 @@
 df_final$stateArea<-ifelse(df_final$stateName==state.name,state.area)
 df_final$long<-ifelse(df_final$stateName==state.name,state.center[["x"]])
 df_final$lat<-ifelse(df_final$stateName==state.name,state.center[["y"]])
+
+str(df_final)
