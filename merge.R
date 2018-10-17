09e18bc8686e8d67d3b4c1580ee096037f565f4c
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 18:35:26 -0400

    Saving merge.R

--- merge.R
+++ merge.R
@@ -2,6 +2,6 @@
 #aasign USArrests dataset to a new dataframe
 arrests<-USArrests
 #Merge arrests and census dataset
-df_final<-merge(x=census,y=arrests,by.x="stateName",by.y="row.names")
-str(df_final)
+df_merge<-merge(x=census,y=arrests,by.x="stateName",by.y="row.names")
+str(df_merge)
 
