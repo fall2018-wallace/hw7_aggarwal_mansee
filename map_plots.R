9b631d5667f8e1af3703af46bd699463df0a9abc
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 22:40:20 -0400

    Saving map_plots.R

--- map_plots.R
+++ map_plots.R
@@ -6,17 +6,18 @@ map_areaColor<-ggplot(df_final,aes(map_id=stateName)) +
            geom_map(map=us,aes(fill=stateArea)) +
            expand_limits(x=df_final$long,y=df_final$lat) + 
            coord_map()+
+           ggtitle("state map of US based on area")
 
 #plotting color coded map based on Murder Rate
 map_murderColor<-ggplot(df_final,aes(map_id=stateName)) +
   geom_map(map=us,aes(fill=Murder)) +
   expand_limits(x=df_final$long,y=df_final$lat) + 
   coord_map()+
-  ggtitle()
+  ggtitle("state map of US based on murder rate")
   
 #plotting color coded map based on Murder rate and population as circle per state
 map_population<-map_murderColor +
-  geom_point(data=df_final,aes(x=long,y=lat,size=population))
+  geom_point(data=df_final,aes(x=long,y=lat,size=population))+
   
 #showing only north-east region for color coded map based on murder rate
 zooMap_murderColor<-ggplot(df_final,aes(map_id=stateName)) +
