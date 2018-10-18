4293c293ad0ef9aacb1c05a18195bbcd7fb4f2c2
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 22:30:47 -0400

    Saving add_area_center.R

--- add_area_center.R
+++ add_area_center.R
@@ -3,6 +3,7 @@
 df_final$stateArea<-ifelse(df_final$stateName==state.name,state.area)
 #comparing statenames and then assigning lonngitude value to each state in df_final dataset
 df_final$long<-ifelse(df_final$stateName==state.name,state.center[["x"]])
+#comparing statenames and then assigning latitude value to each state in df_final dataset
 df_final$lat<-ifelse(df_final$stateName==state.name,state.center[["y"]])
 
 str(df_final)
