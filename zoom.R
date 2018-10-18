cb62b45db7c29e40c1823e2c5677b522fd8b58c7
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 22:17:03 -0400

    Saving zoom.R

--- zoom.R
+++ zoom.R
@@ -1,7 +1,7 @@
 
 long_nyc<-df_final[which(df_final$stateName=="new york"),10]
 lat_nyc<-df_final[which(df_final$stateName=="new york"),11]
-
+which(df_final$stateName=="new york")
 long_nyc
 lat_nyc
 
