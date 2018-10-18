a7272c2c39987832dd75bc9b209f3e6810a6b858
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 22:33:14 -0400

    Saving zoom.R

--- zoom.R
+++ zoom.R
@@ -1,5 +1,6 @@
 
 #fetching cenetrs of new york state
+# x value (longitude) of new york state
 long_nyc<-df_final[which(df_final$stateName=="new york"),10]
 lat_nyc<-df_final[which(df_final$stateName=="new york"),11]
 which(df_final$stateName=="new york")
